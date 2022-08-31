0842ea8e91c89ca1a3221ac8aeffc205fcaeab91
Pedro Roque <pedro.roque@outlook.com>
2022-08-31 20:36:25 -0300

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import React from "react";
 export const App = () => {
   return (
     <>
-      <h1>Hello world!</h1>
-      <span>Parcel Rocks!</span>
+      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+      <div className="grid mt-40">
+        <span className="text-center text-5xl text-red-800 shadow-blue-500 drop-shadow-lg ">Parcel Rocks!</span>
+        <span className="text-center text-5xl text-red-800">Parcel Rocks!</span>
+      </div>
     </>
   );
 };
