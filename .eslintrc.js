0bf4d1c24825c81659ed37447bbee70e26bfb06f
Pedro Roque <pedro.roque@outlook.com>
2022-10-24 18:09:07 -0300

    add testing library

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,21 +3,14 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript'
-  ],
-  overrides: [
-  ],
+  extends: ['plugin:react/recommended', 'standard-with-typescript'],
+  overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: './tsconfig.json'
   },
-  plugins: [
-    'react'
-  ],
-  rules: {
-  }
-}
+  plugins: ['react'],
+  rules: {}
+};
---
+++ src/App.spec.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { screen } from '@testing-library/dom'
+import { App } from './App'
+
+describe('App', () => {
+  beforeEach(() => render(<App />))
+
+  it('should render', () => {
+    expect(screen.getByText(/Hello World/i)).toBeDefined()
+  })
+})
