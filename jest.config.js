824aec9a0565cb695b39ed13a5d88b77100416b1
Pedro Roque <pedro.roque@outlook.com>
2022-11-07 12:05:15 -0300

    change report format to sonar

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   testEnvironment: 'jsdom',
   collectCoverage: true,
   coverageReporters: ['lcov'],
-  reporters: ['jest-silent-reporter', 'summary', ['jest-sonar', { outputDirectory: 'reports', outputName: 'frontend-tests.xml' }]]
+  reporters: ['jest-silent-reporter', 'summary', ['jest-sonar', { outputDirectory: 'reports/sonar', outputName: 'frontend.xml' }], ['jest-junit', { outputDirectory: 'reports/junit', outputName: 'frontend.xml' }]]
 }
